bb9a2f193849e0118604f2406ce825c078001392
Paul Fournel <paul.fournel@gmail.com>
2023-05-01 07:50:53 +0200

    fixing router

--- src/app/PrivateRoute.js
+++ src/app/PrivateRoute.js
@@ -8,5 +8,5 @@ export const PrivateRoute = () => {
         return <div>Loading...</div>;
     }
 
-    return user ? <Outlet/> : <Navigate to="/login"/>;
+    return user.domain ? <Outlet/> : <Navigate to="/login"/>;
 };
